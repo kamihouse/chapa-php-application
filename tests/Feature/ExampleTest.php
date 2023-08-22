a03d5ad85a14210b69fa8ba88b16458deded1d98
Mateus Macedo Dos Anjos <mateus.anjos@fretebras.com.br>
2023-08-22 09:36:15 -0300

    feat: 🎉 project scaffolding

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+describe('example', function () {
+    it('may should be true', function () {
+        expect(true)->toBeTrue();
+    });
+});
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+describe('example', function () {
+    it('may should be true', function () {
+        expect(true)->toBeTrue();
+    });
+});
